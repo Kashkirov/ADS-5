26dd7511d102a4eeaf349047c61957074b8a6a83
Kirill Kashkirov <124998794+Kashkirov@users.noreply.github.com>
2023-04-02 10:56:10 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -3,7 +3,10 @@
 #define INCLUDE_ALG_H_
 
 #include <string>
-
+enum class Operation : char { 
+  Add = '+', Subtract = '-', Multiply = '*',Divide = '/', OpenBracket = '(', CloseBracket = ')' 
+};
+int GetPriority(const char& operation);
 std::string infx2pstfx(std::string);
 int eval(std::string);
 
