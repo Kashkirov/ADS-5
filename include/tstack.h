7518830237151d28ae5cc9da50efd16d34f1cb90
Kirill Kashkirov <124998794+Kashkirov@users.noreply.github.com>
2023-04-02 10:43:16 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,42 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private: 
+    T arr[size];
+    int top;
+public:
+    TStack() :top(-1) {}
+    void Push(const T& value)
+    {
+        if (top >= size) {
+            throw std::string("Stack has already been filled !");
+        } else {
+            arr[++top] = value;
+        }
+    }
+    T GetValue() const {
+        if (top == -1) {
+            throw std::string("Stack has no element within it");
+        } else {
+            return arr[top];
+        }
+    }
+    T Pop() {
+        if (top == -1) {
+            throw std::string("Stack has no element within it");
+        } else {
+            return arr[top--];
+        }
+    }
+    void ForceRemove() {
+        top--;
+    }
+    bool IsFull() const {
+        return top == size - 1;
+    }
+    bool IsEmpty() const{
+        return top == -1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
