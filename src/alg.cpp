7402ec331705785ea8c54eec16474d37b0b0116f
Kirill Kashkirov <124998794+Kashkirov@users.noreply.github.com>
2023-04-02 10:41:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,11 +4,75 @@
 #include "tstack.h"
 
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+  std::string post;
+    TStack<char, 50> op;
+    for (char j: inf) {
+        if (j != '(' && j != ')' && j != '*' && j != '/' && j != '+' && j != '-') {
+            post.push_back(j);
+            post.push_back(' ');
+        } else {
+            if (GetPriority(j) == 0 || op.IsEmpty() || GetPriority(op.GetValue()) < GetPriority(j)) {
+                op.Push(j);
+            } else if (j == ')') {
+                    while ('(' != op.GetValue()) {
+                        post.push_back(op.Pop());
+                        post.push_back(' ');
+                    }
+                    op.ForceRemove();
+            } else {
+                    bool sign = true;
+                    while (sign && !op.IsEmpty()) {
+                        sign = GetPriority(j) <= GetPriority(op.GetValue());
+                        post.push_back(op.Pop());
+                        post.push_back(' ');
+                    }
+                    op.Push(j);
+                }
+        }
+    }
+    while (!op.IsEmpty()) {
+        post.push_back(op.Pop());
+        post.push_back(' ');
+    }
+    post.pop_back();
+    return post;
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+  int result=0;
+    int Exploited;
+    int Domain;
+    TStack<int, 50> num;
+    for (char j : pref) {
+        if (j == '(' || j == ')' || j == '*' || j == '/' || j == '+' || j == '-') {
+            switch (static_cast<int>(j)) {
+            case static_cast<int>(Operation::Add):
+                Exploited = num.Pop();
+                Domain = num.Pop();
+                num.Push(Exploited + Domain);
+                break;
+            case static_cast<int>(Operation::Subtract):
+                Exploited = num.Pop();
+                Domain = num.Pop();
+                num.Push (Domain - Exploited);
+                break;
+            case static_cast<int>(Operation::Multiply):
+                Exploited = num.Pop();
+                Domain = num.Pop();
+                num.Push(Exploited * Domain);
+                break;
+            case static_cast<int>(Operation::Divide):
+                Exploited = num.Pop();
+                Domain = num.Pop();
+                num.Push(Domain / Exploited);
+                break;
+            }
+        } else if (static_cast<int>(j) != 32 && static_cast<int>(j) != 40 && static_cast<int>(j) != 41) {
+            num.Push(ConvertCharNumbers(j));
+        }
+    }
+    if (!num.IsEmpty()) {
+        result = num.Pop();
+    }
+    return result;
 }
